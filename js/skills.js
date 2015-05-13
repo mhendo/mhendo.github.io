35030bfaa73d26f83afc0b5e716c54f435d4273f
mhendo <mike@suretybonds.com>
2015-05-13 16:24:16 -0500

    Home & Skills Page Responsive
    Woo!!! :)

--- js/skills.js
+++ js/skills.js
@@ -1,4 +1,4 @@
-$('.icons').on('mouseenter', function(){
+/*$('.icons').on('mouseenter', function(){
 	console.log('hover')
 	$(this).children('img').addClass('faded');
 	$(this).children('.content1, .content2').fadeIn('fast');
@@ -7,5 +7,17 @@ $('.icons').on('mouseenter', function(){
 $('.icons').on('mouseleave', function(){
 	console.log('hover')
 	$(this).children('img').removeClass('faded');
+	$(this).children('.content1, .content2').fadeOut('fast');
+});*/
+
+$('.icons').on('mouseenter', function(){
+	console.log('hover')
+	$(this).children('.desktop-icon').addClass('faded');
+	$(this).children('.content1, .content2').fadeIn('fast');
+});
+
+$('.icons').on('mouseleave', function(){
+	console.log('hover')
+	$(this).children('.desktop-icon').removeClass('faded');
 	$(this).children('.content1, .content2').fadeOut('fast');
 });
