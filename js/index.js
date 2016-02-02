c1af07d86de610a9e2d317567fc5427735d90ab3
mhendo <mike@suretybonds.com>
2016-02-01 19:00:09 -0600

    HP Edits

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-//click on an icon
-$('.hp-icons a').on('click', function(e){
-	e.preventDefault();
-	//grab the icon's url
-	var pickedIcon = $(this).attr('href');
-	//hide other content
-	$('.content').slideUp();
-	//show slowly
-	$(pickedIcon).slideToggle('slow');
-});
+//remove form field value on click
+$(".formfield").on("click", function(event){
+        $(this).val("");
+    });
